3aca1971c07ca43aea5a495df47716ddf2332304
Priscillia Ejiro <ejirogoro27@gmail.com>
2026-04-13 10:25:58 +0100

    Updated Data Preprocessing

--- src/shopease_app/data_preprocessing.py
+++ src/shopease_app/data_preprocessing.py
@@ -6,7 +6,7 @@ import os
 
 from sklearn.model_selection import train_test_split
 
-from config.constant import Input_Data, model_name, truncation, max_length, padding, Train_Data, Test_Data
+from config.constant import Input_Data, Cleaned_Data, model_name, truncation, max_length, padding, Train_Data, Test_Data
 from src.shopease_app.data_cleaning import clean_data
 logging.basicConfig(level=logging.INFO)
 
@@ -16,8 +16,18 @@ logging.basicConfig(level=logging.INFO)
 
 class DataProcessor:
     def __init__(self):
-        # Load already cleaned data 
-        self.data = clean_data(pd.read_csv(Input_Data))
+        try:
+            if os.path.exists(Cleaned_Data):
+                logging.info("Loading cleaned data from file...")
+                self.data = pd.read_csv(Cleaned_Data)
+            else:
+                logging.info("Cleaned data not found. Running cleaning pipeline...")
+                raw_data = pd.read_csv(Input_Data)
+                self.data = clean_data(raw_data)
+
+        except Exception as e:
+            logging.error(f"Error loading data: {e}")
+            raise
 
     def split_data(self):
         try:
@@ -48,7 +58,7 @@ class DataProcessor:
 class Tokenizer:
     def __init__(self):
         self.tokenizer = BertTokenizer.from_pretrained(model_name)
-
+        
     def encode(self, text):
         return self.tokenizer(
             text.tolist(),
@@ -58,7 +68,13 @@ class Tokenizer:
             return_tensors="pt"
         )
 
+    def save(self, path="./sentiment_model"):
+        os.makedirs(path, exist_ok=True)
+        self.tokenizer.save_pretrained(path)
+        logging.info("Tokenizer saved successfully.")
 
+
+# DATASET CLASS
 
 class SentimentDataset(torch.utils.data.Dataset):
     def __init__(self, encodings, labels):
@@ -96,19 +112,20 @@ def prepare_sentiment_data():
         train_dataset = SentimentDataset(train_encodings, y_train)
         test_dataset = SentimentDataset(test_encodings, y_test)
 
-        logging.info(f"Data successfully prepared.")
+        # SAVE DATASETS
         os.makedirs(os.path.dirname(Train_Data), exist_ok=True)
         os.makedirs(os.path.dirname(Test_Data), exist_ok=True)
         torch.save(train_dataset, Train_Data)
         torch.save(test_dataset, Test_Data)
 
-        # SAVED IN A PY TORCH FORMAT
+        tokenizer.save("./sentiment_model")
+        
+        logging.info(f"Data Successfully Prepared and Saved.")
         return train_dataset, test_dataset
 
     except Exception as e:
         logging.error(f"Error occurred while preparing dataset: {e}")
         raise
-
 
 
 # ENTRY POINT
